4fa42f56bb58276a8703d2f67e00c506542a9c0e
Luca Guth <lucaguth@web.de>
2022-06-12 00:22:16 +0200

    unfinished changes for Board

--- src/Logic/Board.cpp
+++ src/Logic/Board.cpp
@@ -18,5 +18,24 @@ Board::CellState Board::getCell(uint16_t x, uint16_t y) {
 }
 
 Board::TurnResult Board::dropCoin(uint16_t column, CellState state) {
+    assert(column < width);
+    assert(state != CellState::EMPTY);
+    for (int i = 0; i < height; i++) {
+        if (getCell(column, i) == CellState::EMPTY) {
+            m_cells[i*height + column] = state;
+            return checkWin(column, i) ? TurnResult::VALID_WIN : TurnResult::VALID;
+        }
+    }
     return TurnResult::INVALID;
+}
+
+bool Board::checkWin(uint16_t x, uint16_t y) {
+    assert(x < width && y < height);
+
+    // calculate borders for check
+    uint16_t left   = (int32_t)x - 3 < 0         ? 0        : x - 3;
+    uint16_t right  = (int32_t)x + 3 >= width-1  ? width-1  : x + 3;
+    uint16_t bottom = (int32_t)y - 3 < 0         ? 0        : y - 3;
+    uint16_t top    = (int32_t)y + 3 >= height-1 ? height-1 : y + 3;
+
 }
