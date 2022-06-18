e1d8e484cfdfb62d15f130d88ebe42b028adf5ed
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2022-06-18 16:01:15 +0200

    fix infinite recursion in RandomBot.cpp

--- src/logic/RandomBot.cpp
+++ src/logic/RandomBot.cpp
@@ -3,6 +3,6 @@
 
 uint16_t RandomBot::makeMove(const Game &game) {
     uint16_t x = rand() % game.getBoard().width;
-    if (game.getBoard().getCell(x, 0) == Board::CellState::EMPTY) return x;
+    if (game.getBoard().getCell(x, game.getBoard().height-1) == Board::CellState::EMPTY) return x;
     return makeMove(game);
 }
