fde9a61efb072c4fe14df736c3522317e868b4cd
LubaLP <admin@lubalp.eu>
2022-06-20 13:16:13 +0200

    fix ColumBot

--- src/logic/actors/ColumnBot.cpp
+++ src/logic/actors/ColumnBot.cpp
@@ -1,19 +1,13 @@
 #include "ColumnBot.h"
 
 uint16_t ColumnBot::makeMove(const Game &game) {
-    uint16_t highestColumn = 0;
-    uint16_t highestColumnHeight = 0;
-    for (uint16_t x = 0; x < game.getBoard().width; x++) {
-        for (uint16_t y = 0; y < game.getBoard().height; y++) {
+    for (uint16_t y = game.getBoard().height-2; true; y--) {
+        for (uint16_t x = 0; x < game.getBoard().width; x++) {
             if (game.getBoard().getCell(x, y) != Board::CellState::EMPTY) {
-                if (y > highestColumnHeight && y < game.getBoard().height - 1) {
-                    if (y == game.getBoard().height-2) return x;
-                    highestColumn = x;
-                    highestColumnHeight = y;
-                    break;
-                }
+		    if (game.getBoard().getCell(x, y+1) == Board::CellState::EMPTY) {
+			    return x;
+		    }
             }
         }
     }
-    return highestColumn;
 }
