c154bd413b1d45ecb3347e53ba9d3eb87d666ef9
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2022-06-20 14:11:39 +0200

    fix column bot (4 real this time)

--- src/logic/actors/ColumnBot.cpp
+++ src/logic/actors/ColumnBot.cpp
@@ -1,13 +1,15 @@
 #include "ColumnBot.h"
+#include <cassert>
 
 uint16_t ColumnBot::makeMove(const Game &game) {
-    for (uint16_t y = game.getBoard().height-2; true; y--) {
+    for (uint16_t y = game.getBoard().height; y != 0; y--) {
         for (uint16_t x = 0; x < game.getBoard().width; x++) {
-            if (game.getBoard().getCell(x, y) != Board::CellState::EMPTY) {
-		    if (game.getBoard().getCell(x, y+1) == Board::CellState::EMPTY) {
-			    return x;
-		    }
+            if (y-2 < 0 || game.getBoard().getCell(x, y-2) != Board::CellState::EMPTY) {
+                if (game.getBoard().getCell(x, y-1) == Board::CellState::EMPTY) {
+                    return x;
+                }
             }
         }
     }
+    assert(false && "ColumnBot::makeMove: no empty cell found -> invalid board state");
 }
